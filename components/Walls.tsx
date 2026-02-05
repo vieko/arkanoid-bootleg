9e39e197b483547f1893e1e1ddf7ea5a4bbfdb2f
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2026-02-05 09:46:01 -0700

    Use meshBasicMaterial for walls to render exact color

--- components/Walls.tsx
+++ components/Walls.tsx
@@ -11,19 +11,19 @@ export default function Walls() {
       {/* Left wall */}
       <mesh position={[-GAME_WIDTH / 2 - wallThickness / 2, 0, 0]}>
         <boxGeometry args={[wallThickness, GAME_HEIGHT + wallThickness * 2, wallDepth]} />
-        <meshStandardMaterial color={COLORS.walls} />
+        <meshBasicMaterial color={COLORS.walls} />
       </mesh>
 
       {/* Right wall */}
       <mesh position={[GAME_WIDTH / 2 + wallThickness / 2, 0, 0]}>
         <boxGeometry args={[wallThickness, GAME_HEIGHT + wallThickness * 2, wallDepth]} />
-        <meshStandardMaterial color={COLORS.walls} />
+        <meshBasicMaterial color={COLORS.walls} />
       </mesh>
 
       {/* Top wall */}
       <mesh position={[0, GAME_HEIGHT / 2 + wallThickness / 2, 0]}>
         <boxGeometry args={[GAME_WIDTH + wallThickness * 2, wallThickness, wallDepth]} />
-        <meshStandardMaterial color={COLORS.walls} />
+        <meshBasicMaterial color={COLORS.walls} />
       </mesh>
 
       {/* Bottom boundary (invisible, just for reference) */}
