6bd795b1618f2b26ab15e3bc735e2284cd9395e5
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2026-02-05 09:43:38 -0700

    Change wall color to white

--- lib/constants.ts
+++ lib/constants.ts
@@ -39,7 +39,7 @@ export const COLORS = {
   ball: '#ffff00',
   brickColors: ['#ff0055', '#ff6600', '#ffff00', '#00ff66', '#00ffff'],
   background: '#0a0a1a',
-  walls: '#2d2d4a',
+  walls: '#ffffff',
 };
 
 // Power-ups
