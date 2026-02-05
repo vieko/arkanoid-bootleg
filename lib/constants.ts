f3dd295eeb77081e47d2cbb0fc7148457459093e
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2026-02-05 09:41:21 -0700

    Make wall color more visible (#2d2d4a)

--- lib/constants.ts
+++ lib/constants.ts
@@ -39,7 +39,7 @@ export const COLORS = {
   ball: '#ffff00',
   brickColors: ['#ff0055', '#ff6600', '#ffff00', '#00ff66', '#00ffff'],
   background: '#0a0a1a',
-  walls: '#1a1a3a',
+  walls: '#2d2d4a',
 };
 
 // Power-ups
